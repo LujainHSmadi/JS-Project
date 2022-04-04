cb3ff0a084828a95470d9560b88f6b7ae537165a
LujainHSmadi <100359593+LujainHSmadi@users.noreply.github.com>
2022-04-04 15:58:05 +0300

    main

--- task/21-3-2022/task2/part5.js
+++
@@ -1,9 +0,0 @@
-let x = window.prompt("please enter a number")
-let y = window.prompt("please enter a another number")
-
-if(x>y){
-     window.alert("Hello World" )
-}else{
-   
-    window.alert("Goodbye")
-}
--- task/23-3-2022/task6.js
+++
@@ -1,6 +0,0 @@
-let songName = document.getElementById('songName');
-let submit = document.getElementById('submit')
-
-function addSong(){
-    document.getElementById('main').innerHTML +=  songName.value +`<br>`;
-}
